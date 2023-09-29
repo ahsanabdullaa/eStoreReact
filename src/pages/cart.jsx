4dfbd3ce302592a84fa84bc5ca29a2b3c4b4afc1
ahsanabdullaa <73553412+ahsanabdullaa@users.noreply.github.com>
2023-09-29 17:03:36 +0500

    Update cart.jsx

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Delete } from "@mui/icons-material"; // Import Material-UI icons
 
 export default function Cart() {
   // Initialize cart state with an empty array
